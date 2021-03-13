fdeebe05be6b075180ceb55a070bd95d94a6d436
VictorAlberoni <victor.alberoni1201@gmail.com>
2021-03-13 17:19:58 -0300

    Updated int Array test class

--- src/main/kotlin/one/digitalinovation/TesteIntArray.kt
+++ src/main/kotlin/one/digitalinovation/TesteIntArray.kt
@@ -12,23 +12,22 @@ fun main() {
     for (valor in values){
         println(valor)
     }
+
     println("______________")
     values.forEach { valor ->
         println(valor)
     }
+
     println("______________")
     for (index in values.indices){
         println(values[index])
     }
+
     println("______________")
     values.sort()
     for (valor in values){
         println(valor)
     }
-    println("______________")
-    values.sort()
-    for (valor in values){
-        println(valor)
-    }
+
 
 }
