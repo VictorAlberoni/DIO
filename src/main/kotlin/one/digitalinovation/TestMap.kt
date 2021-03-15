a1e3318c5c013a60a25eb7d11db8fa185d6bc47c
VictorAlberoni <victor.alberoni1201@gmail.com>
2021-03-15 15:43:00 -0300

    Criado TestMap.kt

---
+++ src/main/kotlin/one/digitalinovation/TestMap.kt
@@ -0,0 +1,14 @@
+package one.digitalinovation
+
+fun main() {
+    val pair: Pair<String, Double> = Pair("João", 1000.0)
+    val map1 = mapOf(pair)
+
+    map1.forEach { (k, v) -> println("Chave: $k - Valor: $v") }
+    println("-----------------")
+
+    val map2 = mapOf("Pedro" to 2000.0, "Maria" to 5950.0)
+    map2.forEach { (k, v) -> println("Chave: $k - Valor: $v") }
+    println("-----------------")
+
+}
