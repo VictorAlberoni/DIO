41fb9cdfde74af44019ba961eadefea94e189a81
VictorAlberoni <victor.alberoni1201@gmail.com>
2021-03-15 15:12:44 -0300

    Extraida a data class funcionario() do arquivo TestList.kt e criada em um arquivo separado funcionario.kt

--- src/main/kotlin/one/digitalinovation/TestList.kt
+++ src/main/kotlin/one/digitalinovation/TestList.kt
@@ -19,15 +19,5 @@ fun main() {
 
 }
 
-data class funcionario(
-    val nome: String,
-    val salario: Double,
-    val tipoContrato: String
-){
-    override fun toString(): String =
-        """
-            Nome: $nome
-            Salario: R$ $salario
-        """.trimIndent()
-}
+
 
