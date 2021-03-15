82125841ce0a1a3449f9f2b5a1926bece6288752
VictorAlberoni <victor.alberoni1201@gmail.com>
2021-03-15 14:26:37 -0300

    Update OperationArray test(any{}, find{} & count{}) class

--- src/main/kotlin/one/digitalinovation/TesteOperacoes.kt
+++ src/main/kotlin/one/digitalinovation/TesteOperacoes.kt
@@ -15,7 +15,22 @@ fun main() {
     println("---------------")
 
     val  salariosMaiorQue2500 = salarios.filter { it > 2500 }
-
     salariosMaiorQue2500.forEach{println(it)}
+    println("---------------")
+
+    println(salarios.count { it in 2000.0 .. 5000.0 })
+    println("---------------")
+
+    println(salarios.find { it == 2550.0 })
+    println("---------------")
+    println(salarios.find { it == 250.0 })
+    println("---------------")
+
+    println(salarios.any { it == 1000.0 })
+    println(salarios.any { it == 500.0 })
+    println("---------------")
+
+
+
 
 }
