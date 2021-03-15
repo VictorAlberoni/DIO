6e314fda8a87c023cad8f43f86346a19b74ff586
VictorAlberoni <victor.alberoni1201@gmail.com>
2021-03-15 14:59:45 -0300

    Atualizado TestList.kt, utilizando .sortedBy{it.salario}.forEach{println()} foi organizado e impressa a lista em forma crescente de acordo com o salario

--- src/main/kotlin/one/digitalinovation/TestList.kt
+++ src/main/kotlin/one/digitalinovation/TestList.kt
@@ -5,11 +5,14 @@ fun main() {
     val maria = funcionario("Maria", 4750.0)
     val pedro = funcionario("Pedro", 1500.0)
 
-    val funcionarios = listOf(joao, pedro, maria)
+    val funcionarios = listOf(joao, maria, pedro)
 
     funcionarios.forEach{println(it)}
     println("----------------------")
     println(funcionarios.find { it == maria })
+    println("----------------------")
+
+    funcionarios.sortedBy { it.salario }.forEach { println(it) }
 }
 
 data class funcionario(
